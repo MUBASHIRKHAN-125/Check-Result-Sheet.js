d3d3e34fec71f0f65b64a248b7da3742f4a4ebf4
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2024-11-30 15:11:53 +0500

    added

--- script.js
+++ script.js
@@ -1,32 +1,68 @@
 function check1(){
-    let phy1 = Number(document.getElementById("phy1").value);
-    let phy2 = Number(document.getElementById("phy2").value);
-    document.getElementById("phytotal").innerText = phy1;
-    document.getElementById("phyobtain").innerText = phy2;
-    document.getElementById("per1").innerText = (phy2/phy1)* 100;
-    let chem1 = Number(document.getElementById("chem1").value);
-    let chem2 = Number(document.getElementById("chem2").value);
-      document.getElementById("chytotal").innerText = chem1;
-    document.getElementById("chyobtain").innerText = chem2;
-    document.getElementById("per2").innerText = (chem2/chem1)* 100;
-    let math1 = Number(document.getElementById("math1").value);
-    let math2 = Number(document.getElementById("math2").value);
-      document.getElementById("mattotal").innerText = math1;
-    document.getElementById("matobtain").innerText = math2;
-    document.getElementById("per3").innerText = (math2/math1)* 100;
-    document.getElementById("totalmark").innerText = phy1 + chem1 + math1;
-    document.getElementById("totalsecured").innerText = phy2 + chem2 + math2;
-    document.getElementById("totalpercentage").innerText = (phy2 + chem2 + math2) / (phy1 + chem1 + math1) * 100;
-    }
-    let percentage = 80;
-if(percentage>=80){
-    document.getElementById("totalgrade").innerText = "A+"
+    let phyTotal = Number(document.getElementById("phy1").value);
+    let phyObtained = Number(document.getElementById("phy2").value);
+    let physicsNumberOfPercentage =  (phyObtained / phyTotal) * 100;
+    document.getElementById("phytotal").innerText = phyTotal;
+    document.getElementById("phyobtained").innerText = phyObtained;
+    document.getElementById("Phyper").innerText = physicsNumberOfPercentage + "%";
+   if (physicsNumberOfPercentage>=33){
+    document.getElementById("Physicsgrade").innerText = "PASS"
+   }
+   else{
+    document.getElementById("Physicsgrade").innerText = "FAIL"
+   }
+    let chemTotal = Number(document.getElementById("chem1").value);
+    let chemObtained = Number(document.getElementById("chem2").value);
+    let chemistryNumberOfPercentage =  (chemObtained / chemTotal) * 100;
+    document.getElementById("chytotal").innerText = chemTotal;
+    document.getElementById("chyobtain").innerText = chemObtained;
+    document.getElementById("chemper").innerText = chemistryNumberOfPercentage + "%";
+   if (chemistryNumberOfPercentage>=33){
+    document.getElementById("chemgrade").innerText = "PASS"
+   }
+   else{
+    document.getElementById("chemgrade").innerText = "FAIL"
+   }
+    let mathTotal = Number(document.getElementById("math1").value);
+    let mathObtained = Number(document.getElementById("math2").value);
+    let mathNumberOfPercentage =  (mathObtained / mathTotal) * 100;
+    document.getElementById("mattotal").innerText = mathTotal;
+    document.getElementById("matobtained").innerText = mathObtained;
+    document.getElementById("mathper").innerText = mathNumberOfPercentage + "%";
+   if (mathNumberOfPercentage>=33){
+    document.getElementById("mathgrade").innerText = "PASS"
+   }
+   else{
+    document.getElementById("mathgrade").innerText = "FAIL"
+   }
+   let totalNumberOfAllSubject = phyTotal + chemTotal + mathTotal;
+   let obtainedNumberOfAllSubject = phyObtained + chemObtained + mathObtained;
+   let totalPercentageOfAllSubject =  (obtainedNumberOfAllSubject / totalNumberOfAllSubject) * 100;
+   document.getElementById("totalallmark").innerText =  totalNumberOfAllSubject;
+   document.getElementById("totalallsecured").innerText = obtainedNumberOfAllSubject;
+   document.getElementById("totalallpercentage").innerText = totalPercentageOfAllSubject + "%";
+   if(totalPercentageOfAllSubject>=80){
+        document.getElementById("totalgrade").innerText = "A+";
 }
-else if(percentage>=70){
-   document.getElementById("totalgrade").innerText = "B"
+   else if (totalPercentageOfAllSubject>=70){
+        document.getElementById("totalgrade").innerText = "A";
 }
-else if(percentage>=60){
-    document.getElementById("totalgrade").innerText = "C"
+  else  if (totalPercentageOfAllSubject>=60){
+        document.getElementById("totalgrade").innerText = "B"
 }
+  else  if (totalPercentageOfAllSubject>=50){
+        document.getElementById("totalgrade").innerText = "C"
+}
+  else  if (totalPercentageOfAllSubject>=40){
+        document.getElementById("totalgrade").innerText = "D"
+}
+  else  if (totalPercentageOfAllSubject>=33){
+        document.getElementById("totalgrade").innerText = "E"
+}
+  else{
+        document.getElementById("totalgrade").innerText = "FAIL"
+}
+}
+
 
 
